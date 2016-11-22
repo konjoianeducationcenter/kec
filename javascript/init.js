7c2cf19a57973e3497c5120ebfc01351d7cce828
konjoianeducationcenter <konjoianeducationcenter@gmail.com>
2016-11-22 18:40:38 -0500

    First Upload
    Ready to go José

---
+++ javascript/init.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    'use strict';
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
+    $('.modal-trigger').leanModal();
+    $('ul.tabs').tabs();
+});
